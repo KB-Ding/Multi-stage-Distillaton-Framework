a00a6d65e197151f8a374d18fd14e87255275227
KunBo-Ding <49431052+KB-Ding@users.noreply.github.com>
2021-11-29 14:44:04 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch \
---nproc_per_node=8 \
---master_port 29502 train.py \
---config /apdcephfs/share_1157269/karlding/mul_sentence_transformers/config/Multilingual/albert_bottleneck_ver4.config \
---do_test \
---distributed \
---logdir /apdcephfs/share_1157269/karlding/mul_output_train
